cf8f6aea6772e1ef2750d81831054b202f5b9bcb
AndrewVolosevich <volosevich1989@gmail.com>
2020-12-17 22:02:49 +0300

    fix

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,19 +1,22 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UsePipes } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthDto } from './dto/auth.dto';
+import { ValidationPipe } from '../pipes/validation.pipe';
 
-@Controller('api/ auth')
+@Controller('api/auth')
 export class AuthController {
   constructor(
     private authService: AuthService
   ) {}
 
   @Post('login')
+  @UsePipes(ValidationPipe)
   async login(@Body() authDto: AuthDto) {
     return this.authService.loginUser(authDto)
   }
 
   @Post('register')
+  @UsePipes(ValidationPipe)
   async register(@Body() authDto: AuthDto) {
     return this.authService.registerUser(authDto)
   }
--- src/pipes/validation.pipe.ts
+++ src/pipes/validation.pipe.ts
@@ -10,7 +10,7 @@ import { plainToClass } from 'class-transformer';
 @Injectable()
 export class ValidationPipe implements PipeTransform<any> {
   async transform(value: any, { metatype }: ArgumentMetadata) {
-    if (!metatype || !this.toValidate(metatype)) {
+    if (!metatype || !ValidationPipe.toValidate(metatype)) {
       return value;
     }
     const object = plainToClass(metatype, value);
@@ -18,10 +18,14 @@ export class ValidationPipe implements PipeTransform<any> {
     if (errors.length > 0) {
       throw new BadRequestException('Validation failed');
     }
+
+    if(typeof value.email === 'string' && value.email) {
+      value.email = value.email.toLowerCase()
+    }
     return value;
   }
 
-  private toValidate(metatype: Function): boolean {
+  private static toValidate(metatype: Function): boolean {
     const types: Function[] = [String, Boolean, Number, Array, Object];
     return !types.includes(metatype);
   }
